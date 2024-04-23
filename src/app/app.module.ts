42793be7a74456b352bb0d97c7391dc0dd15e613
Jhon Gómez Marín <gmezjhon@gmail.com>
2024-04-22 19:01:34 -0500

    New children routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { ProgressComponent } from './pages/progress/progress.component';
 import { Grafica1Component } from './pages/grafica1/grafica1.component';
 import { AppRoutingModule } from './app-routing.module';
+import { PagesComponent } from './pages/pages.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { AppRoutingModule } from './app-routing.module';
     FooterComponent,
     SidebarComponent,
     ProgressComponent,
-    Grafica1Component
+    Grafica1Component,
+    PagesComponent
   ],
   imports: [
     BrowserModule,
