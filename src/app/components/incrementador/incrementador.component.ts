5e051a3fbf0ccacc83fc4ce3a5caadb4b67f64dc
Jhon Gómez Marín <gmezjhon@gmail.com>
2024-05-06 20:46:00 -0500

    ngClass buttons

--- src/app/components/incrementador/incrementador.component.ts
+++ src/app/components/incrementador/incrementador.component.ts
@@ -1,16 +1,23 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-incrementador',
   templateUrl: './incrementador.component.html',
   styleUrls: ['./incrementador.component.css']
 })
-export class IncrementadorComponent {
+export class IncrementadorComponent implements OnInit {
 
 	// El componente PADRE es progress
 	@Input() progreso: number = 50;
+	@Input() btnClass: string = 'btn-primary';
 
 	@Output() valorSalida: EventEmitter<number> = new EventEmitter();
+
+
+	ngOnInit(): void {
+		
+		this.btnClass = `btn ${this.btnClass}`
+	}
 
 	cambiarValor( valor: number ) {
 
