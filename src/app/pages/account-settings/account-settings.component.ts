33dc6a2ebd8acd262d3e746b9022b35c28b88ab6
Jhon Gómez Marín <gmezjhon@gmail.com>
2024-05-15 20:01:38 -0500

    Type of data

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class AccountSettingsComponent implements OnInit {
 	
 	public linkTheme = document.querySelector("#theme");
-	public links!: any;
+	public links!: NodeListOf<Element>;
 
 	ngOnInit(): void {
 		this.links = document.querySelectorAll(".selector")
