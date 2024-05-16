75c4f3886e90b816a8fca0cd00c7302817d95f71
Jhon Gómez Marín <gmezjhon@gmail.com>
2024-05-15 20:47:43 -0500

    Sidebar service

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { SidebarService } from 'src/app/services/sidebar.service';
 
 @Component({
-  selector: 'app-sidebar',
-  templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
+	selector: 'app-sidebar',
+	templateUrl: './sidebar.component.html',
+	styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
+	
+	private sidebarService = inject( SidebarService )
 
+	menuItems!: any[];
+
+	ngOnInit(): void {
+		//Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+		//Add 'implements OnInit' to the class.
+		this.menuItems = this.sidebarService.menu;
+		console.log(this.menuItems);
+	}
 }
